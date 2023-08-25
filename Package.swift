3d0a2e6e6c3f9c0bcddbe113b23ba1f56e3d10d4
Andreas von Holy <andreasv@tfg.co.za>
2023-08-25 15:25:30 +0200

    fuck this

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.4.3")
+        .package(url: "https://github.com/scinfu/SwiftSoup.git", "2.4.2"..<"2.4.3")
     ],
     targets: [
         // Main library
